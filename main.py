ec95a37fc5bc3e503b54071a8f9a3e28140056f1
72436-glitch <al072436@uacam.mx>
2025-11-18 12:17:57 -0600

    main

---
+++ main.py
@@ -0,0 +1,9 @@
+from funciones import pruebas
+from interfaz import iniciar_gui
+
+def main():
+    pruebas()        # Corre pruebas internas
+    iniciar_gui()    # Abre la interfaz gráfica
+
+if __name__ == "__main__":
+    main()
